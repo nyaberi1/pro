c965c00f8dc82ef31521c4ecf25637cacd2a106a
nyaberi1 <kjoshuan001@gmail.com>
2023-11-21 10:56:22 +0300

    version 1

---
+++ resources/views/pages/enrollment.blade.php
@@ -0,0 +1,40 @@
+<x-guest-layout>
+
+
+    
+
+    {{-- <form method="POST" action="{{ route('enrollment') }}">
+        @csrf
+        
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+        <div>
+            <x-input-label for="gender" :value="__('Gender')" />
+            <x-text-input id="gender" class="block mt-1 w-full" type="text" name="gender" :value="old('gender')" required autofocus autocomplete="gender" />
+            <x-input-error :messages="$errors->get('gender')" class="mt-2" />
+                
+        </div>
+        <div>
+            <x-input-label for="country" :value="__('Country')" />
+            <x-text-input id="country" class="block mt-1 w-full" type="text" name="country" :value="old('country')" required autofocus autocomplete="country" />
+            <x-input-error :messages="$errors->get('country')" class="mt-2" />
+        </div>
+        <div>
+            <x-input-label for="faculty" :value="__('Faculty')" />
+            <x-text-input id="faculty" class="block mt-1 w-full" type="text" name="faculty" :value="old('faculty')" required autofocus autocomplete="faculty" />
+            <x-input-error :messages="$errors->get('faculty')" class="mt-2" />
+        </div> --}}
+
+
+
+
+
+</x-guest-layout>
